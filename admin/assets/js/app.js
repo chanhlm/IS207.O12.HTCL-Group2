cff26be95d5d7392701b7ff84f81b57bca92e044
chanhlm <minhchanh.lmc@gmail.com>
2023-11-04 00:03:12 +0700

    update database and struct file of dashboard admin

--- admin/assets/js/app.js
+++ admin/assets/js/app.js
@@ -1,37 +1,33 @@
-$(function() {
+$(function () {
 	"use strict";
-	
-	
-	$(".mobile-search-icon").on("click", function() {
+	$(".mobile-search-icon").on("click", function () {
 		$(".search-bar").addClass("full-search-bar")
-	}), $(".search-close").on("click", function() {
+	}), $(".search-close").on("click", function () {
 		$(".search-bar").removeClass("full-search-bar")
-	}), $(".mobile-toggle-menu").on("click", function() {
+	}), $(".mobile-toggle-menu").on("click", function () {
 		$(".wrapper").addClass("toggled")
-	}), $(".toggle-icon").click(function() {
-		$(".wrapper").hasClass("toggled") ? ($(".wrapper").removeClass("toggled"), $(".sidebar-wrapper").unbind("hover")) : ($(".wrapper").addClass("toggled"), $(".sidebar-wrapper").hover(function() {
+	}), $(".toggle-icon").click(function () {
+		$(".wrapper").hasClass("toggled") ? ($(".wrapper").removeClass("toggled"), $(".sidebar-wrapper").unbind("hover")) : ($(".wrapper").addClass("toggled"), $(".sidebar-wrapper").hover(function () {
 			$(".wrapper").addClass("sidebar-hovered")
-		}, function() {
+		}, function () {
 			$(".wrapper").removeClass("sidebar-hovered")
 		}))
-	}), $(document).ready(function() {
-		$(window).on("scroll", function() {
+	}), $(document).ready(function () {
+		$(window).on("scroll", function () {
 			$(this).scrollTop() > 300 ? $(".back-to-top").fadeIn() : $(".back-to-top").fadeOut()
-		}), $(".back-to-top").on("click", function() {
+		}), $(".back-to-top").on("click", function () {
 			return $("html, body").animate({
 				scrollTop: 0
 			}, 600), !1
 		})
-	}), $(function() {
-		for (var e = window.location, o = $(".metismenu li a").filter(function() {
-				return this.href == e
-			}).addClass("").parent().addClass("mm-active"); o.is("li");) o = o.parent("").addClass("mm-show").parent("").addClass("mm-active")
-	}), $(function() {
+	}), $(function () {
+		for (var e = window.location, o = $(".metismenu li a").filter(function () {
+			return this.href == e
+		}).addClass("").parent().addClass("mm-active"); o.is("li");) o = o.parent("").addClass("mm-show").parent("").addClass("mm-active")
+	}), $(function () {
 		$("#menu").metisMenu()
 	});
-	
-
-
-
-
 });
+
+
+
