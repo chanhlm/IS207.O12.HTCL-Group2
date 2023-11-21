ff1898dba2d43e0c7d297b908e27db22ae808a82
chanhlm <minhchanh.lmc@gmail.com>
2023-11-21 20:08:17 +0700

    split component for admin

---
+++ admin/components/sidebar.php
@@ -0,0 +1,96 @@
+<div class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header text-center">
+        <div><img src="../../shared/img/logo-icon.png" class="logo-icon px-20" alt="logo icon"></div>
+        <div><h4 class="logo-text">ecoTech</h4></div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i></div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+        <li>
+            <a href="./dashboard.php">
+                <div class="parent-icon"><i class='bx bx-home-circle'></i></div>
+                <div class="menu-title">Dashboard</div>
+            </a>
+        </li>
+        <hr>
+        <li>
+            <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class='bx bx-category'></i></div>
+                <div class="menu-title">Danh mục sản phẩm</div>
+            </a>
+            <ul>
+                <li> <a href="./categories.php"><i class="bx bx-list-ul"></i>Danh sách</a></li>
+                <li> <a href="./categories-insert.php"><i class="bx bx-add-to-queue"></i>Thêm danh mục</a></li>
+            </ul>
+        </li>
+        <li>
+            <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class="bx bx-package"></i></div>
+                <div class="menu-title">Sản phẩm</div>
+            </a>
+            <ul>
+                <li> <a href="./product.php"><i class="bx bxs-package"></i>Danh sách sản phẩm</a></li>
+                <li> <a href="./product-import.php"><i class="bx bx-import"></i>Nhập kho</a></li>
+                <li> <a href="./product-export.php"><i class="bx bx-export"></i>Xuất kho</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class='bx bxs-user-account'></i></div>
+                <div class="menu-title">Người dùng</div>
+            </a>
+            <ul>
+                <li> <a href="./customer.php"><i class="bx bx-user-pin"></i>Khách hàng</a></li>
+                <li> <a href="./employee.php"><i class="bx bxs-user-badge"></i>Nhân viên</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a class="has-arrow" href="javascript:;">
+                <div class="parent-icon"><i class='bx bxs-receipt'></i></div>
+                <div class="menu-title">Đơn hàng</div>
+            </a>
+            <ul>
+                <li> <a href="./order.php"><i class="bx bx-list-ul"></i>Danh sách đơn hàng</a></li>
+                <li> <a href="./order.php"><i class="bx bx-add-to-queue"></i>Tạo hóa đơn</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="./warranty-page.php">
+                <div class="parent-icon"> <i class='bx bxs-hourglass-top'></i></div>
+                <div class="menu-title">Bảo hành</div>
+            </a>
+        </li>
+
+        <li>
+            <a href="./blog-page.php">
+                <div class="parent-icon"> <i class="bx bxl-blogger"></i></div>
+                <div class="menu-title">Blog</div>
+            </a>
+        </li>
+
+        <li>
+            <a href="./promotion-page.php">
+                <div class="parent-icon"> <i class="bx bxs-discount"></i></div>
+                <div class="menu-title">Khuyến mãi</div>
+            </a>
+        </li>
+
+        <li>
+            <a href="./feedback.php">
+                <div class="parent-icon"> <i class="bx bx-comment-detail"></i></div>
+                <div class="menu-title">Phản hồi</div>
+            </a>
+        </li>
+
+        <li>
+            <a href="../../public/views/index.php">
+                <div class="parent-icon"> <i class="bx bx-store"></i></div>
+                <div class="menu-title">Về cửa hàng</div>
+            </a>
+        </li>
+    </ul>
+    <!--end navigation-->
+</div>
